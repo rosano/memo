5e85c72b0da8b8ee0e1f336fd68568a243a8fbd4
Rosano Coutinho <github.com@xeari.com>
2026-01-09 11:16:48 +0100

    fix typo

--- vite.config.js
+++ vite.config.js
@@ -22,9 +22,13 @@ export default defineConfig({
 			'loc.sv',
 		],
 	},
-	exclude: [
-		'svelte-codemirror-editor',
-		'codemirror',
-		'@codemirror/language-markdown',
-	],
+	// https://github.com/touchifyapp/svelte-codemirror-editor
+	// If you try to use this component with vite or svelte-kit, you have to disable dependency optimization for codemirror and all its extensions.
+  optimizeDeps: {
+  	exclude: [
+  		'svelte-codemirror-editor',
+  		'codemirror',
+  		'@codemirror/language-markdown',
+  	],
+  },
 });
