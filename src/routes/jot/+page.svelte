791b19dfeee2747dbeeeefd2922cb9ec067a5bab
Rosano Coutinho <github.com@xeari.com>
2026-01-09 10:56:30 +0100

    tidy

--- src/routes/jot/+page.svelte
+++ src/routes/jot/+page.svelte
@@ -199,7 +199,15 @@ import Feedback from '$lib/Feedback.svelte';
 </article>
 
 <footer>
-	<CodeMirror bind:value={ mod._description } lang={ markdown() } lineNumbers={ false } highlight={ {activeLine: false,} } placeholder="what are you thinking?" keybindings={ ['Mod-Enter', 'Control-Enter', 'Alt-Enter'].map(key => ({key, run: mod.Enter })) } onready={ e => {  mod._textarea = e; setTimeout(() => mod._textarea.focus(), 300) } } lineWrapping={ true } />
+	<CodeMirror
+		bind:value={ mod._description } 
+		lang={ markdown() } 
+		lineNumbers={ false } 
+		highlight={ {activeLine: false,} } 
+		placeholder="what are you thinking?" 
+		keybindings={ ['Mod-Enter', 'Control-Enter', 'Alt-Enter'].map(key => ({key, run: mod.Enter })) } 
+		onready={ e => { mod._textarea = e; setTimeout(() => mod._textarea.focus(), 300) } }
+		lineWrapping={ true } />
 	<button class="jot-add" disabled={ mod.descriptionEmpty() ? true : null }
 		onmouseup={ mod.touchignore }
 		onmousedown={ mod.touchignore }
